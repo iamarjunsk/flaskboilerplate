6d68209e63255d9b5c2922882ba5f486af803110
Arjun SK <dev.arjunsk@gmail.com>
2020-07-18 22:25:58 +0530

    base

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/yt'
-from secretes import appkey
+from app.secretes import appkey
 
 app.secret_key = appkey
 
